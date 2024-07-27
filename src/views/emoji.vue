c0323cce9293103bbec8bdbed8ca6159d9fa82da
simin.chen <moriachan@163.com>
2024-07-27 18:28:08 +0800

    feat: emoji

--- src/views/emoji.vue
+++ src/views/emoji.vue
@@ -13,34 +13,53 @@
       </el-input>
     </div>
     <ul v-infinite-scroll="load" :infinite-scroll-disabled="disabled" class="infinite-list">
-      <li v-for="i in count" :key="i" class="infinite-list-item">
+      <li v-for="(item,index) in countList" :key="index" class="infinite-list-item">
         <el-card>
           <el-image
-              :src="randomUrl(i)"
+              :src="randomUrl(index)"
               :zoom-rate="1.2"
               :max-scale="7"
               :min-scale="0.2"
-              :preview-src-list="[randomUrl(i)]"
+              :preview-src-list="[randomUrl(index)]"
               :initial-index="0"
-              fit="fill"
+              fit="contain"
           />
+          <div class="icon-box">
+            <el-icon color="red" @click="item.like++">
+              <IconBxsLike />
+            </el-icon>
+            <p>{{ item.like }}</p>
+            <el-icon color="#409eff" @click="item.dislike++">
+              <IconBxsDislike />
+            </el-icon>
+            <p>{{ item.dislike }}</p>
+          </div>
         </el-card>
       </li>
     </ul>
-    <div style="text-align: center">
+    <div class="center-text">
       <p v-if="loading">Loading...</p>
       <p v-if="noMore">No more</p>
     </div>
+    <el-pagination
+        :page-size="20"
+        :pager-count="11"
+        layout="prev, pager, next"
+        :total="1000"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
 import axios from 'axios';
 import { ref } from 'vue';
+import IconBxsLike from '~icons/bxs/like'
+import IconBxsDislike from '~icons/bxs/dislike'
 
 axios.defaults.baseURL = 'https://nsfw.just4dream.club';
 
 const count = ref(0);
+const countList = ref([]);
 const loading = ref(false);
 const noMore = computed(() => count.value >= 100);
 const disabled = computed(() => loading.value || noMore.value);
@@ -48,7 +67,12 @@ const searchVal = ref();
 const load = () => {
   loading.value = true;
   setTimeout(() => {
-    count.value += 10;
+    const newRow = {
+      like: 0,
+      dislike: 0
+    };
+    const arr = new Array(10).fill().map(() => Object.assign({}, newRow));
+    countList.value = [...countList.value,...arr]
     loading.value = false;
   }, 500);
 };
@@ -74,10 +98,20 @@ const randomUrl = (i: number) => {
 <style lang="scss" scoped>
 .image-container{
   width: 100%;
-  height: 95vh;
+  height: 90vh;
   padding-top: 80px;
   box-sizing: border-box;
   position: relative;
+  .center-text{
+    height: 20px;
+    font-size: 14px;
+    margin: 10px 0;
+    text-align: center;
+    p{
+      margin:0;
+      padding: 0;
+    }
+  }
   .search-input{
     position: absolute;
     top: 30px;
@@ -105,13 +139,30 @@ const randomUrl = (i: number) => {
     }
     :deep(.el-card__body){
       height: 100%;
-      padding: 20px;
+      padding: 30px 20px 40px 20px;
       box-sizing: border-box;
       text-align: center;
       .el-image{
         height: 100%;
       }
+      .icon-box{
+        font-size: 15px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        margin-top: 5px;
+        p{
+          margin: 0 10px 0 5px;
+        }
+      }
+      .el-icon{
+        font-size: 20px;
+        cursor: pointer;
+      }
     }
+  }
+  :deep(.el-pagination){
+    justify-content: center;
   }
 }
 </style>
